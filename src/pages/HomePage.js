a5550697b9060739fb06d99507f859a4aced68c6
Yaroslavaa <104118022+Yaroslavaa@users.noreply.github.com>
2024-12-18 21:26:52 +0500

    commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ import './HomePage.css';
 const HomePage = () => {
   return (
     <div className="home-container">
-      <h1 className="gallery-title">Приложение для авторской программы "Интеграция уроков образовательной области «Язык и литература» с дисцип-линами искусствоведческого цикла"</h1>
+      <h1 className="gallery-title">Приложение для авторской программы "Интеграция уроков образовательной области «Язык и литература» с дисциплинами искусствоведческого цикла"</h1>
       <div className="gallery">
         {paintings.map((painting) => (
           <PaintingCard key={painting.id} painting={painting} />
